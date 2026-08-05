af36f498e5e2f5e2c7b7e3bfa3d208a595399536
Zac Downie <14842@holbertonstudents.com>
2026-08-05 14:20:32 +1000

    base file

---
+++ test/_printf.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	unsigned int i;
+
+	/**
+	* grabbed from character after percentage
+	*/
+	switch (format[i])
+	{
+		case 'c':
+		case 's':
+		case '%':
+	}
+}
