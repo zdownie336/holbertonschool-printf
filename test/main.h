259e62833f36f95ec9143020c60e33ae95a99319
Malakai <gam3rsuknight@gmail.com>
2026-08-05 15:01:28 +1000

    write function

--- test/main.h
+++ test/main.h
@@ -2,5 +2,7 @@
 #define _main_H_
 
 int _printf(const char *format, ...);
+int stdwr(char *c);
+int _strlen(char *s);
 
 #endif /* _main_H_ */
---
+++ test/wrtest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	stdwr("test\n");
+
+	return (0);
+}
