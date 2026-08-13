9641af84923ceddf6ae6852209fe158b019aa4a2
Zac Downie <14842@holbertonstudents.com>
2026-08-13 15:55:21 +1000

    setting up string

--- get_func.c
+++ get_func.c
@@ -16,19 +16,26 @@ int (*get_func(char *s))(int, int)
 	func_t vars[] = {
 	{'c', _putchar}
 	{'s', _putchar}
-	{'d', convert}
-	{'i', convert}
-	{'b', convert}
-	{'u', convertu}
-	{'o', convert}
-	{'x', convert}
-	{'X', convert}
+	{'d', convert(va_arg(arg, int), buff, 10)}
+	{'i', convert(va_arg(arg, int), buff, 10)}
+	{'b', convert(va_arg(arg, int), buff, 2)}
+	{'u', convertu(va_arg(arg, long int), buff, 10)}
+	{'o', convert(va_arg(arg, int), buff, 8)}
+	{'x', convert(va_arg(arg, int), buff, 16)}
+	{'X', convert(va_arg(arg, int), buff, 16)}
 	{'%', _putchar}
 	{'\0', NULL}
-}
+};
+	int i;
+
 	i = 0;
 	while (vars[i] != NULL)
 	{
-		if 
+		if (strcmp(s, vars[i].op) == 0)
+		{
+			return (vars[i].f)
+		}
+		i = i + 1;
 	}
+	return (NULL);
 }
